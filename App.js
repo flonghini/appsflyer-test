b262ddd66663765af83789637a652c6bf4582001
Francisco Longhini <francisco.longhini@ckl.io>
2021-02-24 14:50:28 -0300

    added appsflyer, react-native-config

--- App.js
+++ App.js
@@ -1,8 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import appsFlyer from "react-native-appsflyer";
+import Config from "react-native-config";
 
 export default function App() {
+  const devKey = Config.APPS_FLYER_DEV_KEY;
+  const isDebug = Config.APPS_FLYER_IS_DEBUG;
+  const appId = Config.APPS_FLYER_ANDROID_APP_ID;
+
+  const appsFlyerParams = {
+    devKey,
+    isDebug,
+    appId,
+  };
+  if (
+    Object.values(appsFlyerParams).every(
+      (paramValue) => paramValue !== undefined
+    )
+  ) {
+    appsFlyer.initSdk(
+      appsFlyerParams,
+      (result) => {
+        console.log(`AppsFlyer initialization ${result}`);
+      },
+      (error) => {
+        console.error(`AppsFlyer initialization ${error}`);
+      }
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -14,8 +41,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
